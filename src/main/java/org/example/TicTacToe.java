21e5328f18ae784573c8b0aef7406c823f586c8c
petra <petramicudova5@gmail.com>
2024-04-01 22:16:45 +0200

    change of players to "X" and "O"; add new method isNumeric

--- src/main/java/org/example/TicTacToe.java
+++ src/main/java/org/example/TicTacToe.java
@@ -5,10 +5,8 @@ import java.util.Scanner;
 public class TicTacToe {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Player 1, enter your name, please: ");
-        char player1 = scanner.nextLine().charAt(0);
-        System.out.println("Player 2, enter your name, please: ");
-        char player2 = scanner.nextLine().charAt(0);
+        char player1 = 'X';
+        char player2 = 'O';
 
         boolean playAgain = true;
         while (playAgain) {
@@ -21,13 +19,19 @@ public class TicTacToe {
             boolean gameOngoing = true;
             boolean player1Turn = true;
 
-
             while (gameOngoing) {
                 printGameBoard(gameBoard);
                 System.out.println("Player " + (player1Turn ? player1 : player2) + ", enter your move (row[1-3] and column[1-3]): ");
-                int row = scanner.nextInt() - 1;
-                int col = scanner.nextInt() - 1;
+                String rowInput = scanner.next();
+                String colInput = scanner.next();
 
+                if (!isNumeric(rowInput) || !isNumeric(colInput)) {
+                    System.out.println("Invalid input, please enter number within the range [1-3].");
+                    continue;
+                }
+
+                int row = Integer.parseInt(rowInput) - 1;
+                int col = Integer.parseInt(colInput) - 1;
 
                 if (row < 0 || col < 0 || row > 2 || col > 2) {
                     System.out.println("Invalid input, please enter number within the range [1-3].");
@@ -36,7 +40,7 @@ public class TicTacToe {
 
                 if (gameBoard[row][col] == ' ') {
                     gameBoard[row][col] = player1Turn ? player1 : player2;
-                    if (gameOver(gameBoard)) {
+                    if (checkWin(gameBoard)) {
                         gameOngoing = false;
                         System.out.println("Game Over. Player " + (player1Turn ? player1 : player2) + " wins");
                     } else if (isDraw(gameBoard)) {
@@ -71,20 +75,24 @@ public class TicTacToe {
         }
     }
 
-    public static boolean gameOver(char[][] gameBoard) {
+    public static boolean checkWin(char[][] gameBoard) {
+//        checking rows
         for (int i = 0; i < 3; i++) {
             if (gameBoard[i][0] == gameBoard[i][1] && gameBoard[i][0] == gameBoard[i][2] && gameBoard[i][0] != ' ') {
                 return true;
             }
         }
+//        checking columns
         for (int i = 0; i < 3; i++) {
             if (gameBoard[0][i] == gameBoard[1][i] && gameBoard[0][i] == gameBoard[2][i] && gameBoard[0][i] != ' ') {
                 return true;
             }
         }
+//        checking main diagonal
         if (gameBoard[0][0] == gameBoard[1][1] && gameBoard[0][0] == gameBoard[2][2] && gameBoard[0][0] != ' ') {
             return true;
         }
+//        checking secondary diagonal
         if (gameBoard[0][2] == gameBoard[1][1] && gameBoard[0][2] == gameBoard[2][0] && gameBoard[0][2] != ' ') {
             return true;
         }
@@ -100,5 +108,14 @@ public class TicTacToe {
             }
         }
         return true;
+    }
+
+    public static boolean isNumeric(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
     }
 }
